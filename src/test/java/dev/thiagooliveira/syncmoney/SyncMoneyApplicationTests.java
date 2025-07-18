ef492a75cdcd0b541a9313d9207f558ceabb2df2
Thiago Oliveira <thiago@thiagoti.com>
2025-07-18 15:10:52 +0100

    initial commit, added the first dependencies

---
+++ src/test/java/dev/thiagooliveira/syncmoney/SyncMoneyApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.thiagooliveira.syncmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class SyncMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/dev/thiagooliveira/syncmoney/TestSyncMoneyApplication.java
@@ -0,0 +1,11 @@
+package dev.thiagooliveira.syncmoney;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSyncMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SyncMoneyApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
