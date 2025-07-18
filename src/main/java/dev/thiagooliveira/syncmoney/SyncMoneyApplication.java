87e43794524c2e741d2ad23033d1a04af2b0bc31
Thiago Oliveira <thiago@thiagoti.com>
2025-07-18 15:18:31 +0100

    added spotless

--- src/main/java/dev/thiagooliveira/syncmoney/SyncMoneyApplication.java
+++ src/main/java/dev/thiagooliveira/syncmoney/SyncMoneyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SyncMoneyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SyncMoneyApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SyncMoneyApplication.class, args);
+  }
 }
--- src/test/java/dev/thiagooliveira/syncmoney/TestSyncMoneyApplication.java
+++ src/test/java/dev/thiagooliveira/syncmoney/TestSyncMoneyApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestSyncMoneyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(SyncMoneyApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.from(SyncMoneyApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
+  }
 }
