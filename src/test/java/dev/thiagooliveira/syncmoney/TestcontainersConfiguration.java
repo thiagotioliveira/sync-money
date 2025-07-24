4534ce66eb52313e489d26603e1a9ef510a59f3e
Thiago Oliveira <thiago@thiagoti.com>
2025-07-24 19:15:42 +0100

    rollback

--- src/test/java/dev/thiagooliveira/syncmoney/TestcontainersConfiguration.java
+++ src/test/java/dev/thiagooliveira/syncmoney/TestcontainersConfiguration.java
@@ -1,8 +1,5 @@
 package dev.thiagooliveira.syncmoney;
 
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +12,6 @@ public class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   PostgreSQLContainer<?> postgresContainer() {
-    PostgreSQLContainer<?> container =
-        new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
-            .withExposedPorts(5432)
-            .withCreateContainerCmdModifier(
-                cmd ->
-                    cmd.getHostConfig()
-                        .withPortBindings(
-                            new PortBinding(
-                                Ports.Binding.bindPort(5433), // local port
-                                new ExposedPort(5432) // container port
-                                )));
-    return container;
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
   }
 }
