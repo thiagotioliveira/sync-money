bdf6573a898f40ae279d458c4c0ec9a0f0bbafcd
Thiago Oliveira <thiago@thiagoti.com>
2025-07-24 19:12:20 +0100

    fixing database host port

--- src/test/java/dev/thiagooliveira/syncmoney/TestcontainersConfiguration.java
+++ src/test/java/dev/thiagooliveira/syncmoney/TestcontainersConfiguration.java
@@ -1,5 +1,8 @@
 package dev.thiagooliveira.syncmoney;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +15,17 @@ public class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   PostgreSQLContainer<?> postgresContainer() {
-    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    PostgreSQLContainer<?> container =
+        new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+            .withExposedPorts(5432)
+            .withCreateContainerCmdModifier(
+                cmd ->
+                    cmd.getHostConfig()
+                        .withPortBindings(
+                            new PortBinding(
+                                Ports.Binding.bindPort(5433), // local port
+                                new ExposedPort(5432) // container port
+                                )));
+    return container;
   }
 }
