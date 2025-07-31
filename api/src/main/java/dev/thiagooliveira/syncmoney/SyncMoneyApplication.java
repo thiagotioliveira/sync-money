8eeba25c0c0f2d905931c13f6033d57e6e40aad8
Thiago Oliveira <thiago@thiagoti.com>
2025-07-31 15:55:54 +0100

    - update openapi to use bearerAuth - update swagger UI

--- api/src/main/java/dev/thiagooliveira/syncmoney/SyncMoneyApplication.java
+++ api/src/main/java/dev/thiagooliveira/syncmoney/SyncMoneyApplication.java
@@ -1,14 +1,36 @@
 package dev.thiagooliveira.syncmoney;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@OpenAPIDefinition(
-    info = @Info(title = "Sync-Money API", version = "1.0.0", description = "Sync-Money API"))
 @SpringBootApplication
 public class SyncMoneyApplication {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .components(new Components().addSecuritySchemes("bearerAuth", createAPIKeyScheme()))
+        .info(
+            new Info()
+                .title("Sync-Money API")
+                .description("Sync-Money API")
+                .version("1.0.0")
+                .contact(
+                    new Contact()
+                        .name("Thiago Oliveira")
+                        .email("thiago@thiagoti.com")
+                        .url("http://thiagooliveira.dev")));
+  }
+
+  private SecurityScheme createAPIKeyScheme() {
+    return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(SyncMoneyApplication.class, args);
